04c28a19b49d84dbe4d0df361228d42c6a2d6b8e
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2022-03-26 10:12:07 +0200

    creating - ComedyCalculator & TragedyCalculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -8,7 +8,7 @@ export function createStatementData(invoice, plays) {
     return statementData
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance), )
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance)
         result.play = calculator.play
         result.amount = calculator.amount
@@ -42,6 +42,15 @@ export function createStatementData(invoice, plays) {
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay)
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay)
+        default: throw new Error(`unknown type: ${aPlay.type}`)
+    }
+}
+
+
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance
@@ -49,34 +58,34 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0;
-
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience >30) {
-                    result += 1000 * (this.performance.audience - 30);
-                } break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience >20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.performance.play.type}`);
-        }
-
-        return result;
+        throw new Error(`subclass responsibility`);
     }
 
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" == this.play.type) {
-            result += Math.floor(this.performance.audience / 5);
+        return  Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience >30) {
+            result += 1000 * (this.performance.audience - 30);
         }
-        return result;
+        return result
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience >20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
