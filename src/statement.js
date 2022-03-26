5534e328149949dcb6bf664c1925565eb53286f8
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2022-03-26 09:40:44 +0200

    created renderPlainText function, splitted logic for calculation and representation

--- src/statement.js
+++ src/statement.js
@@ -1,40 +1,22 @@
 export function statement(invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-    for (let perf of invoice.performances) {
-        result += ` ${playFor(perf).name}: `;
-        result += `${usd(amountFor(perf))}(${perf.audience} seats)\n`;
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
+    return renderPlainText(createStatementData(invoice, plays))
+}
 
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
+function createStatementData(invoice, plays) {
+    const statementData = {}
+    statementData.customer = invoice.customer
+    statementData.performances = invoice.performances.map(enrichPerformance)
+    statementData.totalAmount = totalAmount(statementData)
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData)
 
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
+    return statementData
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" == playFor(aPerformance).type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance)
+        result.play = playFor(result)
+        result.amount = amountFor(result)
+        result.volumeCredits = volumeCreditsFor(result)
+        return result
     }
 
     function playFor(aPerfоrmance) {
@@ -44,7 +26,7 @@ export function statement(invoice, plays) {
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience >30) {
@@ -58,9 +40,42 @@ export function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+                throw new Error(`unknown type: ${aPerformance.play.type}`);
         }
 
         return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" == aPerformance.play.type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        return result;
+    }
+
+    function totalAmount(data) {
+        return data.performances.reduce((total, p) => total+p.amount, 0)
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total+p.volumeCredits, 0)
+    }
+}
+
+function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: `;
+        result += `${usd(perf.amount)}(${perf.audience} seats)\n`;
+    }
+    result += `Amount owed is ${usd(data.totalAmount)}\n`;
+    result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
     }
 }
