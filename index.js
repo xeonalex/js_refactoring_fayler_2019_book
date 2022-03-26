082d1e36bd7af15d885b0c3b67de0ee0041b115d
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2022-03-26 09:43:29 +0200

    added htmlStatement function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import {statement} from "./src/statement.js";
+import {statement, htmlStatement} from "./src/statement.js";
 import {invoices} from "./data/invoices.js";
 import {plays} from "./data/plays.js";
 
 const invoice = invoices[0]
 
 console.log(statement(invoice, plays))
+console.log(htmlStatement(invoice, plays))
