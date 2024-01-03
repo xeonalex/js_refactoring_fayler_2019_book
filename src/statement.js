78a48d2950f927f83e125286da1da6bdb88c0662
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-03 22:41:10 +0200

    ref/0010: 'function extraction' created format(aNumber)

--- src/statement.js
+++ src/statement.js
@@ -2,8 +2,6 @@ export function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
@@ -52,6 +50,11 @@ export function statement(invoice, plays) {
         if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
 
         return result;
+    }
+
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber)
     }
 }
 
