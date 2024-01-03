5993a65687ac2932c7a37adc6c6d54c78fe2257c
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-03 22:10:01 +0200

    ref/0001: 'function extraction': created amountFor(perf, play) function with switch ... case operator

--- src/statement.js
+++ src/statement.js
@@ -7,24 +7,8 @@ export function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playlD];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error('unknown type: ${play.type}');
-        }
+        let thisAmount = amountFor(perf, play);
+
         // Добавление бонусов
         volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -39,4 +23,27 @@ export function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error('unknown type: ${play.type}');
+    }
+
+    return thisAmount
 }
