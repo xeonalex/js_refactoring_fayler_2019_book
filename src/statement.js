c75989f3c416db09b85feaf285825af62f69d8c8
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-03 22:37:56 +0200

    ref/0009: 'function extraction' / volumeCreditsFor: renamed local variables

--- src/statement.js
+++ src/statement.js
@@ -44,14 +44,14 @@ export function statement(invoice, plays) {
         return result
     }
 
-    function volumeCreditsFor(perf) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
 
         // Дополнительный бонус за каждые 10 комедий
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
 
-        return volumeCredits;
+        return result;
     }
 }
 
