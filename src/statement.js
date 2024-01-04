7655f259d6a052ae1ef3bbf2b0aa3cde37a4cd9e
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-04 11:25:44 +0200

    ref/0020: 1) replaced all usages of amountFor() in renderPlainText() with perf.amount 2) removed amountFor func declaration from renderPlainText()

--- src/statement.js
+++ src/statement.js
@@ -9,26 +9,6 @@ export function statement(invoice, plays) {
         return plays[aPerformance.playlD];
     }
 
-    function enrichPeformance(aPerformance) {
-        const result = Object.assign({}, aPerformance)
-        result.play = playFor(aPerformance)
-        return result
-    }
-}
-
-export function renderPlainText(data) {
-    let result = `Statement for ${data.customer}\n`;
-
-    for (let perf of data.performances) {
-        // Вывод строки счета
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))}`;
-        result += ` (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;
-
     function amountFor(aPerformance) {
         let result = 0;
         switch (aPerformance.play.type) {
@@ -51,6 +31,27 @@ export function renderPlainText(data) {
 
         return result
     }
+
+    function enrichPeformance(aPerformance) {
+        const result = Object.assign({}, aPerformance)
+        result.play = playFor(result)
+        result.amount = amountFor(result)
+        return result
+    }
+}
+
+export function renderPlainText(data) {
+    let result = `Statement for ${data.customer}\n`;
+
+    for (let perf of data.performances) {
+        // Вывод строки счета
+        result += ` ${perf.play.name}: ${usd(perf.amount)}`;
+        result += ` (${perf.audience} seats)\n`;
+    }
+
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
@@ -79,7 +80,7 @@ export function renderPlainText(data) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
 
         return result
