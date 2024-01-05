627783369d29f0c22cc6914c874d1f51ee8d0dcb
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-05 11:37:15 +0200

    ref/0026: 1) added htmlStatement method

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import {statement} from "./src/statement.js";
+import {htmlStatement, statement} from "./src/statement.js";
 import {invoices} from "./data/invoices.js";
 import {plays} from "./data/plays.js";
 
 const invoice = invoices[0]
 
-const result = statement(invoice, plays)
-console.log(result)
+const resultText = statement(invoice, plays)
+console.log(resultText)
 
+const resultHtml = htmlStatement(invoice, plays)
+console.log(resultHtml)
 
-const expectedResult = `Statement for BigCo
+const expectedTextResult = `Statement for BigCo
  Hamlet: $650.00 (55 seats)
  As You Like It: $580.00 (35 seats)
  Othello: $500.00 (40 seats)
@@ -16,9 +18,25 @@ Amount owed is $1,730.00
 You earned 47 credits
 `
 
-if (result === expectedResult) {
-    console.info('SUCCESS')
-} else {
-    console.error('FAILED')
-}
+const expectedHtmlResult = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>amount</th></tr>
+<tr><td>Hamlet</td><td>55</td><td>$650.00<td></tr>
+<tr><td>As You Like It</td><td>35</td><td>$580.00<td></tr>
+<tr><td>Othello</td><td>40</td><td>$500.00<td></tr>
+</table>
+<p>Amount owed is <em>$1,730.00</em></p>
+<p>You earned <em>47</em> credits</p>
+`
 
+console.info('RESULT:')
+printResult('text', resultText, expectedTextResult)
+printResult('html', resultHtml, expectedHtmlResult)
+
+function printResult(representationType, result, expected) {
+    if (result === expected) {
+        console.info(`${representationType}: SUCCESS`)
+    } else {
+        console.error(`${representationType}: FAILED`)
+    }
+}
--- src/statement.js
+++ src/statement.js
@@ -8,13 +8,31 @@ export function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
-        // Вывод строки счета
         result += ` ${perf.play.name}: ${usd(perf.amount)}`;
         result += ` (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
+    return result;
+}
+
+export function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays))
+}
+
+export function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>amount</th></tr>\n";
+    for (let perf of data.performances) {
+        result += `<tr><td>${perf.play.name}</td>`;
+        result += `<td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}<td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
     return result;
 }
 
