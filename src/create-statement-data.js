abd54eddc47daedeb6c54c1d7fbc16987c739c44
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-05 12:01:53 +0200

    ref/0032: 1) 'replace constructor with factory function' - added createPerformanceCalculator()

--- src/create-statement-data.js
+++ src/create-statement-data.js
@@ -31,8 +31,8 @@ export default function createStatementData(invoice, plays) {
     }
 
     function enrichPeformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
         const result = Object.assign({}, aPerformance)
-        const calculator = new PerformanceCalculator(aPerformance, playFor(result))
         result.play = calculator.play
         result.amount = calculator.amount
         result.volumeCredits = calculator.volumeCredits
@@ -77,5 +77,16 @@ class PerformanceCalculator {
         if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
 
         return result;
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case 'tragedy': return new TragedyCalculator(aPerformance, aPlay)
+        case 'comedy': return new ComedyCalculator(aPerformance, aPlay)
+        default: throw new Error(`unknown type: ${aPlay.type}`)
     }
 }
