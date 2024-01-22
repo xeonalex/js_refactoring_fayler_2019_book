e7b1bc446fdae17fee6d97b48e7940ae318c9bd6
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-01-22 10:26:45 +0200

    0003/test: test for profit value

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
-import assert from 'assert'
+import {expect} from 'chai'
 import {Province, sampleProvinceData} from "../index.js";
 
 describe('province', function () {
+    let asia;
+    beforeEach(function () {
+        asia = new Province(sampleProvinceData())
+    })
+
     it('shortfall', function () {
-        const asia = new Province(sampleProvinceData())
-        assert.equal(asia.shortfall, 5)
+        expect(asia.shortfall).equal(5)
+    });
+
+    it('profit', function () {
+        expect(asia.profit).equal(230)
     });
 });
